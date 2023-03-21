ce524506b5d2a031330e5bd1df019a13954dead7
Dmitry Iv <df.creative@gmail.com>
2023-03-21 12:41:17 -0400

    Make banners shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
       backgroundImage: {
         bannerApples: "url(../assets/banner-apples.png)",
         bannerRoses: "url(../assets/banner-roses.png)",
-        stripes: "linear-gradient(108deg, #ff6161 21.95%, #d44e4e 21.95%, #d44e4e 50%, #ff6161 50%, #ff6161 71.95%, #d44e4e 71.95%, #d44e4e 100%);"
+        stripes: "linear-gradient(108deg, #ff6161 21.95%, #d44e4e 21.95%, #d44e4e 50%, #ff6161 50%, #ff6161 71.95%, #d44e4e 71.95%, #d44e4e 100%);",
+        shadow: "radial-gradient(closest-side, rgba(0,0,0,.5), rgba(0,0,0,0));"
       }
     },
   },
