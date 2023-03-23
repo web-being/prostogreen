b329fa9c8f644a87a5620dcb6c77dd4b924f4f8b
Dmitry Iv <df.creative@gmail.com>
2023-03-23 12:05:24 -0400

    make login & catalog pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
+  content: ["./src/**/*.{html,js}", "./**/*.html"],
   theme: {
     fontFamily: {
       'sans': ['montserrat','ui-sans-serif', 'system-ui'],
